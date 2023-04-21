5fc5cf2d6507dd8021de110bbe0c4c5228e1611a
Jaem Dessources <jaem.developer@gmail.com>
2023-04-21 10:47:24 -0400

    test

--- src/pages/api/middlewares/authorize.ts
+++ src/pages/api/middlewares/authorize.ts
@@ -4,17 +4,18 @@ import { verifyAccessToken } from "@/utils/jwt";
 export default function authorize(next: NextApiHandler) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
     const apiAccessToken = req.headers.authorization?.split(" ")[1];
-
+    let username;
     let reason;
     try {
       if (!apiAccessToken) {
         throw new Error();
       }
       reason = "token invalid";
-      const { username } = verifyAccessToken(<string>apiAccessToken) as {
+      const payload = verifyAccessToken(<string>apiAccessToken) as {
         username: string;
       };
-      res.status(200).json({ message: "what now ?", username, apiAccessToken });
+      username = payload.username;
+      // res.status(200).json({ message: "what now ?", username, apiAccessToken });
       /** Attach the payload to the request object for later use */
       if (username === "system") return await next(req, res);
       else throw new Error();
@@ -23,6 +24,8 @@ export default function authorize(next: NextApiHandler) {
       console.error(error);
       return res.status(403).json({
         message: "Not authorized ",
+        username,
+        apiAccessToken,
       });
     }
   };
