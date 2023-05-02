ebee5453a99fbe0ad25a7ee305b834d1f7763b42
Jaem Dessources <jaem.developer@gmail.com>
2023-05-01 21:00:20 -0400

    modified formatTime tests so that is doesn't take timezones into account

--- __tests__/utils/formatTime.test.ts
+++ __tests__/utils/formatTime.test.ts
@@ -1,29 +1,28 @@
 import formatTime from "@/utils/formatTime";
 
 beforeAll(() => {
-  process.env.TZ = "America/New_York";
+  process.env.TZ = "Etc/UTC";
 });
 
 describe("formatTime", () => {
   test("formats a timestamp in the morning", () => {
-    const result = formatTime("2022-01-02T14:30:00.000Z");
-    const date = new Date("2022-01-02T14:30:00.000Z");
-    console.log(date.toUTCString());
+    const result = formatTime("2022-01-02T09:30:00.000");
+
     expect(result).toBe("9:30 AM");
   });
 
   test("formats a timestamp in the afternoon", () => {
-    const result = formatTime("2022-01-02T21:30:00.000Z");
+    const result = formatTime("2022-01-02T16:30:00.000");
     expect(result).toBe("4:30 PM");
   });
 
   test("formats a timestamp at midnight", () => {
-    const result = formatTime("2022-01-02T05:00:00.000Z");
+    const result = formatTime("2022-01-02T24:00:00.000");
     expect(result).toBe("12:00 AM");
   });
 
   test("formats a timestamp at noon", () => {
-    const result = formatTime("2022-01-01T17:00:00.000Z");
+    const result = formatTime("2022-01-01T12:00:00.000");
     expect(result).toBe("12:00 PM");
   });
   test("returns an empty string if a timestamp is invalid", () => {
