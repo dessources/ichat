df2ecf973cc6870b19e932165a2b96d119186ed6
Jaem Dessources <jaem.developer@gmail.com>
2023-05-09 20:05:29 -0400

    only connect to socket server if user id is defined

--- src/components/providers/SocketIoProvider.tsx
+++ src/components/providers/SocketIoProvider.tsx
@@ -26,14 +26,16 @@ function SocketIoProvider(props: any) {
 
     //Complete the handshake with the socket.io server
     (async function () {
-      await fetch("/api/socket.io");
-      newSocket = io("/", { query: { roomId: user?.id } });
+      if (user?.id) {
+        await fetch("/api/socket.io");
+        newSocket = io("/", { query: { roomId: user?.id } });
 
-      newSocket.on("connect", () => {
-        process.env.NODE_ENV !== "production" &&
-          console.log("connected to socket.Io server id: ", newSocket?.id);
-        setSocket?.(newSocket as Socket);
-      });
+        newSocket.on("connect", () => {
+          process.env.NODE_ENV !== "production" &&
+            console.log("connected to socket.Io server id: ", newSocket?.id);
+          setSocket?.(newSocket as Socket);
+        });
+      }
     })();
 
     return () => {
