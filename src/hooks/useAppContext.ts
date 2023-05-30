e1b8133cf2c5babd2ac07ee596b12162b20b2a80
jaemdessources <pianodessour es@gmail.com>
2023-05-30 12:20:10 -0400

    test

--- src/hooks/useAppContext.ts
+++ src/hooks/useAppContext.ts
@@ -8,7 +8,7 @@ export default function useAppContext<T = any>(
 ): Context<T> | any {
   const value = React.useContext(context);
 
-  if (!value) {
+  if (value === undefined) {
     console.log(value);
     throw new Error(
       `${context?.displayName} must be used within ${context?.displayName}Provider`
