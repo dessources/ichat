262ff71f307df6e93a3a1364abd9caec5f8a0c43
Jaem Dessources <jaem.developer@gmail.com>
2023-06-08 12:20:17 -0400

    removed unused test file

--- __tests__/index.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import Home from "../src/pages/index";
-
-it("renders homepage unchanged", () => {
-  const { container } = render(<Home />);
-  // code to make test fail on purpose
-  //   expect(4).toBe(5);
-  expect(container).toMatchSnapshot();
-});
